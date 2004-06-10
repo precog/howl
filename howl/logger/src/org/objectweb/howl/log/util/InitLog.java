f93f255a94a9792c8df3ebad47b65f3abcde6e80
girouxm <girouxm@2e47f3c0-6af0-491b-90e8-b007b2e8a2f8>
2004-06-10 21:14:06 +0000

    Initial Checkin.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/howl/trunk@23 2e47f3c0-6af0-491b-90e8-b007b2e8a2f8

---
+++ howl/logger/src/org/objectweb/howl/log/util/InitLog.java
@@ -0,0 +1,18 @@
+/*
+ * JOnAS: Java(TM) Open Application Server
+ * Copyright (C) 2004 Bull S.A.
+ * Contact: jonas-team@objectweb.org
+ * Created on June 7, 2004
+ */
+package org.objectweb.howl.log.util;
+
+/**
+ * @author Michael Giroux
+ *
+ * Initialize set of log files
+ */
+public class InitLog {
+
+	public static void main(String[] args) {
+	}
+}
