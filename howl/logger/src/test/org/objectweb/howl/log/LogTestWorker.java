f7de84ebcf1b31b1bfefba7dc35f1732c6f08e06
girouxm <girouxm@2e47f3c0-6af0-491b-90e8-b007b2e8a2f8>
2004-11-02 21:48:07 +0000

    Modify run() method to print stack trace if exception occurs.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/howl/trunk@227 2e47f3c0-6af0-491b-90e8-b007b2e8a2f8

--- howl/logger/src/test/org/objectweb/howl/log/LogTestWorker.java
+++ howl/logger/src/test/org/objectweb/howl/log/LogTestWorker.java
@@ -103,7 +103,7 @@ public class LogTestWorker extends TestWorker {
         latency += System.currentTimeMillis() - startTime;
       }
     } catch (Exception e) {
-      System.err.println(e.toString());
+      e.printStackTrace();
       exception = e;
     } finally {
       // notify driver that this thread has finishe its work
