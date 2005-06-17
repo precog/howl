50704f1818ca3f33dcf1a7d692b885f64558a0f7
girouxm <girouxm@2e47f3c0-6af0-491b-90e8-b007b2e8a2f8>
2005-06-17 22:27:44 +0000

    update to test new JVM versions.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/howl/trunk@263 2e47f3c0-6af0-491b-90e8-b007b2e8a2f8

--- howl/logger/src/test.cmd
+++ howl/logger/src/test.cmd
@@ -3,34 +3,35 @@ setlocal
 
 set JUNIT_HOME=c:\java\junit3.8.1
 
-set java_opts=%java_opts% -cp bin;%EMMA_HOME%\lib\emma.jar;%JUNIT_HOME%\bin
+set java_opts=%java_opts% -cp .\bin;%EMMA_HOME%\lib\emma.jar;%JUNIT_HOME%\bin
 set java_opts=%java_opts% -showversion -Xnoclassgc -da -dsa
-set java_opts=%java_opts% -server
+set server_opt= -server
 
-set JAVA_HOME=C:\java\j2sdk1.4.2_06
+set JAVA_HOME=C:\java\j2sdk1.4.2_08
 set mode=1.4
 
 if "%1"=="tiger" (
-set JAVA_HOME=C:\java\jdk1.5.0
-set java_opts=%java_opts% -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8888 -Dcom.sun.management.jmxremote.ssl=false
+set JAVA_HOME=C:\java\jdk1.5.0_03
+:: set java_opts=%java_opts% -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8888 -Dcom.sun.management.jmxremote.ssl=false
 set mode=1.5
 )
 
 :: JRockit options that may be valuable
 :: -Xmanagement 
 :: -Djrockit.lockprofiling -XXjra:delay=10,recordingtime=30
+:: set java_opts=%java_opts% -Djrockit.lockprofiling -XXjra:delay=10,recordingtime=30
 
 if "%1"=="jrockit" (
-set JAVA_HOME=C:\java\jrockit-j2sdk1.4.2_04
+set JAVA_HOME=C:\java\jrockit-jdk1.5.0_02
 set mode=1.4
 set PATH=%java_home%\bin;%path%
-set java_opts=%java_opts% -Djrockit.lockprofiling -XXjra:delay=10,recordingtime=30
 )
 
 if "%1"=="ibm" (
 set JAVA_HOME=C:\java\IBM\Java141\jre
 set mode=1.4
 set PATH=%java_home%\jre\bin;%path%
+set server_opt=
 )
 
 set emma=
@@ -40,7 +41,7 @@ set emma=emmarun -r html -sp ./src/java;./src/test -ix -org.objectweb.howl.log.*
 
 pushd ..
 echo on
-%java_home%\bin\java %java_opts% %emma%  org.objectweb.howl.log.LogTest
+%java_home%\bin\java %server_opt% %java_opts% %emma%  org.objectweb.howl.log.ThroughputTest
 @echo off
 popd
 endlocal
