f17947ec927d898f0a4f6052f3d41e67be8ecc07
girouxm <girouxm@2e47f3c0-6af0-491b-90e8-b007b2e8a2f8>
2005-06-23 23:02:41 +0000

    Add $ ID $ comment.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/howl/trunk@271 2e47f3c0-6af0-491b-90e8-b007b2e8a2f8

--- howl/logger/src/java/org/objectweb/howl/log/BlockLogBufferSink.java
+++ howl/logger/src/java/org/objectweb/howl/log/BlockLogBufferSink.java
@@ -30,7 +30,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
- * $ ID: $
+ * $ Id: $
  */
 package org.objectweb.howl.log;
 
